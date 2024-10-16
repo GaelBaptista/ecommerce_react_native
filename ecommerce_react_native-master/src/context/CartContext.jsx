cb1af16e16aa852becbc786b13ea27a5249b002b
Gabriel <rev3npvp@gmail.com>
2024-10-15 21:08:56 -0300

    erros corrigidos

--- ecommerce_react_native-master/src/context/CartContext.jsx
+++ ecommerce_react_native-master/src/context/CartContext.jsx
@@ -1,54 +1,65 @@
-import { createContext, useContext, useState, useEffect } from 'react'
+import { createContext, useContext, useState, useEffect } from "react";
 
 // Criacao do contexto
-const CartContext = createContext()
+const CartContext = createContext();
 
 // Criacao do Provider e exportar funcoes e variaveis
 export const CartProvider = ({ children }) => {
-    const [items, setItems] = useState([])
+  const [items, setItems] = useState([]);
 
-    // Caso for chamar uma API para recuperar o carrinho do usuario salvo no banco de dados
-    useEffect(() => {
-        async function getData() {
-            // await fetch()
-        }
-        getData()
-    }, [])
-    
-    // Numero de itens no carrinho
-    function getTotalItems() {
-        return items.length // [1, 2, 3].length => 3
+  // Caso for chamar uma API para recuperar o carrinho do usuario salvo no banco de dados
+  useEffect(() => {
+    async function getData() {
+      // await fetch()
     }
+    getData();
+  }, []);
 
-    // Add Item
-    function addItem(item) {
-        // [...existente,  novo]
-        setItems(prev => [...prev, item])
-    }
-  
-    function removeItem(id) {
-        // const novoCarrinho = items.filter(item => item.id !== id)
-        // setItems(novoCarrinho)
-        setItems((prevItems) => prevItems.filter(item => item.id !== id))
-    }
+  // Numero de itens no carrinho
+  function getTotalItems() {
+    return items.length; // [1, 2, 3].length => 3 faça isso
+  }
 
-    function valorTotal() {
-        const total = items.reduce((acc, product) => { return acc + product.price }, 0)
-        return total
-    }
+  // Add Item
+  function addItem(item) {
+    // [...existente,  novo]
+    setItems((prev) => [...prev, item]);
+  }
 
-    // Clear Cart    
-    function clearCart() {
-        setItems([])
-    }
+  function removeItem(id) {
+    // const novoCarrinho = items.filter(item => item.id !== id)
+    // setItems(novoCarrinho)
+    setItems((prevItems) => prevItems.filter((item) => item.id !== id));
+  }
 
-    return (
-        <CartContext.Provider value={{ items, getTotalItems, addItem, clearCart, removeItem, valorTotal }}>
-            {children}
-        </CartContext.Provider>
-    )
-}
+  function valorTotal() {
+    const total = items.reduce((acc, product) => {
+      return acc + product.price;
+    }, 0);
+    return total;
+  }
+
+  // Clear Cart
+  function clearCart() {
+    setItems([]);
+  }
+
+  return (
+    <CartContext.Provider
+      value={{
+        items,
+        getTotalItems,
+        addItem,
+        clearCart,
+        removeItem,
+        valorTotal,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
 
 // Criar o hook personalizado
-export const useCart = () => useContext(CartContext)
+export const useCart = () => useContext(CartContext);
 // const { getTotalItems, clearCart } = useCart()
